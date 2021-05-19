04f15a7bbdb3ffdce1a8d79a76fb90b69baaa019
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2021-05-19 18:48:42 +0200

    Update Ejercicio_SM5.js

--- Actividades1 - UT5/Ejercicio_SM5.js
+++ Actividades1 - UT5/Ejercicio_SM5.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let usuario =+ (prompt("Introduce tu nombre: "));
+let usuario =+ (prompt("Introduce tu nombre: ")); //No conviertas a número, te sobra el + delante del prompt
 let mayus;
 
 function compruebaMayus(mayus){
