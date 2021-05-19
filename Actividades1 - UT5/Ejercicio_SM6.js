f137980d93937649051df4ce5598aec98f36f21c
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2021-05-19 18:57:37 +0200

    Update Ejercicio_SM6.js

--- Actividades1 - UT5/Ejercicio_SM6.js
+++ Actividades1 - UT5/Ejercicio_SM6.js
@@ -1,20 +1,20 @@
 'use strict'
 
-let opcion =+ (prompt("Introduce una letra: "));
+let opcion =+ (prompt("Introduce una letra: ")); //Quita el + de delante puesto que es una letra, no lo puedes convertir a número
 let letra;
 
 switch(opcion){
-    case 1:
+    case 1: //Aquí sería case 'a': y tendrías que quitar el if de después
         if(letra == 'a'){
             console.log("AZUL");
         }
     break;
-    case 2:
+    case 2: //Aquí sería case 'v': y tendrías que quitar el if de después
         if(letra == 'v'){
             console.log("VERDE");
         }
     break;
-    case 3:
+    case 3: //Aquí sería case 'r': y tendrías que quitar el if de después
         if(letra == 'r'){
             console.log("ROJO");
         }
